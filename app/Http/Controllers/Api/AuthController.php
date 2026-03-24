2706343b53d6869bcb13fec3d47f12b26552b502
Prasad <prasaddatir05@gmail.com>
2026-03-24 15:48:51 +0530

    feat(auth): implement Sanctum authentication with register, login, and logout

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Container\Attributes\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'User Registered Successfully'
+        ], 201);
+    }
+
+    public function Login(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid Credentials',
+            ], 401);
+        }
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'User Logged In Successfully',
+            'token' => $token
+        ], 200);
+    }
+
+    public function Logout()
+    {
+        auth('sanctum')->user()->currentAccessToken()->delete();
+        return response()->json([
+            'message' => 'User Loggedout successfully',
+        ], 200);
+    }
+}
